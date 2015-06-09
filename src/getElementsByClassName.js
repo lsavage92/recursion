1189e2fb3dd5132ea61e65f212df5a31e280bc66
Luke Savage <luke.savage92@gmail.com>
2015-06-09 12:39:29 -0500

    Created iterative solution for all cases except the last case.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+// You should use document.body, element.childNodes, and element.classList
+var getElementsByClassName = function(className){
+  //Iteratively works for every example except for the last one with the containing divs
+  //need to add a check to see if there are more childNodes. if childNodes[0] === undefined there are no more. This is why we need recursion.
+  var body = document.body;
+  var childNodes = document.body.childNodes;
+  var results = [body];
+  for(var i = 0; i < childNodes.length; i++){
+    if(childNodes[i].classList != undefined){
+        for(var j = 0; j < childNodes[i].classList.length; j++){
+            if(childNodes[i].classList[j] === body.className){
+                results.push(childNodes[i]);
+            }
+        }
+    }
+  }
+
+  return results;
 };
