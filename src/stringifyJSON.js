c1f1de805f346fff97fed629d0b2a1b11aec5a12
Luke Savage <luke.savage92@gmail.com>
2015-06-09 12:45:13 -0500

    Added comments for clarity

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -9,11 +9,13 @@ var stringifyJSON = function(obj) {
   	return;
   }
 
+  // Beginning of string
   var results = "";
 
+  //Handling Arrays
   if(Array.isArray(obj)){
-    results += "[";
-    for(var i = 0; i < obj.length; i++){
+    results += "["; // Formatting
+    for(var i = 0; i < obj.length; i++){ //Loop through every element and add them to the string with a comma except the last element.
         if(i < obj.length - 1){
             results += stringifyJSON(obj[i]) + ",";
         }
@@ -21,36 +23,35 @@ var stringifyJSON = function(obj) {
             results += stringifyJSON(obj[i]);
         }
     }
-    results += "]";
+    results += "]"; //End of formatting
     return results;
   }
   // Check to see if the type is object
   if(typeof obj === "object"){
-  	// null check
-  	if(obj === null){
+  	if(obj === null){ // null check
   		return "null";
   	}
     else{
-      var numProps = countProperties(obj);
+      var numProps = countProperties(obj); //Keep count of properties so we know when we reached the last property in the loop
       var count = 0;
-      results += "{";
-      for(var key in obj){
-        if(typeof obj[key] === "function" || typeof obj[key] === "undefined"){
+      results += "{"; // Formatting
+      for(var key in obj){ // loop through all keys
+        if(typeof obj[key] === "function" || typeof obj[key] === "undefined"){ // if property is function or undefined, add nothing
           results = results;
         }
-        else if(count === numProps - 1){
+        else if(count === numProps - 1){ // adds final property to string 
           results += stringifyJSON(key) + ":" + stringifyJSON(obj[key]);
         }
-        else{
+        else{ // adds all properties to the string except for the last property
           results += stringifyJSON(key) + ":" + stringifyJSON(obj[key]) + ",";
         }
         count++;
       }
-      results += "}";
+      results += "}"; // End of formatting
     }
   }
   else{
-  	//handles primative values
+  	//handles primative values which stringifies all elements in an array, primatives alone or values of the key-value pairs in objects
   	results += handlePrimative(obj);
   }
 
